572b2bc023988200f0103f4f358211ee760e5e66
Sandina Rajapaksha <tech.sandina@gmail>
2026-04-03 01:27:17 +0530

    chore(back-end): setup router and connection

--- backend/server/cmd/api.go
+++ backend/server/cmd/api.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"errors"
+	"net/http"
+)
+
+func main() {
+	//server mux - router that redirect req to correct handler based on pattern
+	router := http.NewServeMux()
+
+	//connection - listen from 8080 and to redirect requests to router
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		errors.New("Cant establish connection")
+	}
+}
